b1f711e6d3e36835a10926cbc7df0397eb5e8ff5
Dot32 <61964090+Dot32IsCool@users.noreply.github.com>
2022-05-15 13:58:41 +0800

    addjusted font sizes and delay

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ fn main() {
 			..Default::default()
 		})
 		.add_plugins(DefaultPlugins)
-		.insert_resource(Progress { time: 0.0 })
+		.insert_resource(Progress { time: -0.2 })
 		.add_startup_system(setup)
 		.add_system(update_dot32_text)
 		.add_system(update_games_text)
@@ -59,7 +59,7 @@ fn setup(mut commands: Commands , asset_server: Res<AssetServer>) {
 				"Dot32",
 				TextStyle {
 					font: asset_server.load("fonts/PT_Sans/PTSans-Bold.ttf"),
-					font_size: 100.0,
+					font_size: 140.0,
 					color: Color::WHITE,
 				},
 				// Note: You can use `Default::default()` in place of the `TextAlignment`
@@ -76,7 +76,7 @@ fn setup(mut commands: Commands , asset_server: Res<AssetServer>) {
 			style: Style {
 					align_self: AlignSelf::Center,
 					position: Rect {
-							bottom: Val::Px(0.0),
+							bottom: Val::Px(20.0),
 							// bottom: Val::Percent(0.5),
 							// right: Val::Percent(50.0),
 							// left: Val::Percent(50.0),
@@ -90,7 +90,7 @@ fn setup(mut commands: Commands , asset_server: Res<AssetServer>) {
 				"Games",
 				TextStyle {
 					font: asset_server.load("fonts/PT_Sans/PTSans-Regular.ttf"),
-					font_size: 45.0,
+					font_size: 50.0,
 					color: Color::WHITE,
 				},
 				// Note: You can use `Default::default()` in place of the `TextAlignment`
@@ -107,7 +107,7 @@ fn setup(mut commands: Commands , asset_server: Res<AssetServer>) {
 
 fn ease_out_elastic(x: f32) -> f32 {
 	let c4 = (2.0*std::f64::consts::PI as f32) / 2.3; // edit "2.3" for effect
-	2.0_f32.powf(-18.0*x) * ((x*10.0 - 0.75)*c4).sin() + 1.0 // edit "-18" for efefct
+	2.0_f32.powf(-18.0*x.max(0.0)) * ((x.max(0.0)*10.0 - 0.75)*c4).sin() + 1.0 // edit "-18" for efefct
 }
 
 fn update_dot32_text(
