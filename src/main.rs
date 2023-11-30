bf713a920d28432d5be17508efb7f07e7574d794
Dot32 <61964090+Dot32IsCool@users.noreply.github.com>
2023-11-30 18:15:42 +0800

    Remove commented out outdated code

--- src/main.rs
+++ src/main.rs
@@ -4,21 +4,11 @@ use bevy::window::*;
 fn main() {
 	App::new()
 		.insert_resource(ClearColor(Color::rgb(0.7, 0.55, 0.41)))
-		// .add_plugins(DefaultPlugins.set(WindowPlugin {
-		// 	window: WindowDescriptor {
-		// 	title: "Dot32 intro".to_string(),
-		// 	width: 800.,
-		// 	height: 600.,
-		// 	present_mode: PresentMode::Fifo, // Vesync enabled, replace Fifo with Mailbox for no vsync
-		// 	..default()
-		// 	},
-		// 	..default()
-		// }))
 		.add_plugins(DefaultPlugins.set(WindowPlugin {
 			primary_window: Some(Window {
 				title: "Dot32 intro".into(),
 				resolution: WindowResolution::new(800., 600.),
-				present_mode: PresentMode::Fifo,
+				present_mode: PresentMode::Fifo, // Vesync enabled, replace Fifo with Mailbox for no vsync
 				..default()
 			}),
 			..default()
