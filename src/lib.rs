f81c0a8738b042765c978e6593a703ced535350d
Dot32 <61964090+Dot32IsCool@users.noreply.github.com>
2023-12-03 17:42:30 +0800

    Fix fixed update

--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,6 @@ use bevy::app::AppExit;
 
 const RESTARTABLE: bool = true;
 const SUBTEXT: &str = "Games";
-const FIXED_TIMESTEP: f64 = 1.0 / 60.0;
 pub const LENGTH: f32 = 1.0;
 pub const FADE: f32 = 0.2;
 
@@ -17,15 +16,13 @@ impl Plugin for Intro {
 	fn build(&self, app: &mut App) {
 		app.insert_resource(Progress { time: -0.2 })
 			.insert_resource(Completed { value: false })
-			.insert_resource(Time::<Fixed>::from_seconds(FIXED_TIMESTEP))
 			.add_systems(Startup, setup)
 			.add_systems(Update, (
 				update_time, 
 				update_background, 
 				delete_when_finished, 
-				keys
-			))
-			.add_systems(FixedUpdate, (
+				keys,
+
 				update_dot32_text, 
 				update_subtext_text, 
 			));
